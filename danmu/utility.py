31baff900873d1d663b1bbd65621bf83036c31ad
heroesm <heroesm@outlook.com>
2017-03-27 19:09:38 +0800

    fix in utility.py:     add threading lock to accommodate thread-unsafe print

--- danmu/utility.py
+++ danmu/utility.py
@@ -9,6 +9,7 @@ import time
 from collections import deque
 
 class Displayer():
+    lock = threading.Lock();
     def __init__(self, nMode=0, nDelay=0):
         # nMode - whether to use threaded display
         # nDelay - the interval time during threaded display
@@ -24,6 +25,7 @@ class Displayer():
             self.thread.start();
             self.display = self.threadedDisplay;
     def commonDisplay(self, *aArgs, **mArgs):
+        type(self).lock.acquire(timeout=5);
         try:
             print(*aArgs, **mArgs);
         except UnicodeEncodeError as e:
@@ -32,6 +34,8 @@ class Displayer():
             #sCode = sys.stdin.encoding;
             #aArgs = (str(x).encode(sCode, 'replace').decode(sCode) for x in aArgs);
             print(*aArgs, **mArgs);
+        finally:
+            type(self).lock.release();
     def dequeue(self):
         while 1:
             while (len(self.queue) > 0):
